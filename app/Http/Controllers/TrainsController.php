9b6f590ceb2ab443d5fc5cf9eeb5cd34722eec12
GiadaTonioni <giada.tonioni@gmail.com>
2023-08-01 20:05:47 +0200

    seeder, faker

--- app/Http/Controllers/TrainsController.php
+++ app/Http/Controllers/TrainsController.php
@@ -4,7 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Train;
+
 class TrainsController extends Controller
 {
-    //
+    public function index(){
+        $trains = Train::where('data_partenza', '>=', now())->get();
+        return view('welcome', compact('trains'));
+    }
 }
---
+++ database/seeders/TrainsTableSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+use Faker\Provider\en_US\Person;
+
+use App\Models\Train;
+
+class TrainsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i = 0; $i<10; $i++){
+            $train = new Train();
+            $train->azienda = $faker->word();
+            $train->stazione_partenza = $faker->word();
+            $train->stazione_arrivo = $faker->word();
+            $train->data_partenza = $faker->date();
+            $train->data_arrivo = $faker->date();
+            $train->orario_partenza = $faker->time();
+            $train->orario_arrivo = $faker->time();
+            $train->codice_treno = $faker->bothify('nt???##');
+            $train->n_carrozze = $faker->numberBetween(1, 20);
+            $train->in_ritardo = $faker->boolean();
+            $train->tempo_ritardo = $faker->time();
+            $train->cancellato = $faker->boolean();
+
+        }
+    }
+}
